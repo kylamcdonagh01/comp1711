0e7def63a024363c643cc0e6b3ad476f455e73be
kylamcdonagh01 <146441430+kylamcdonagh01@users.noreply.github.com>
2023-11-16 09:54:22 +0000

    Steps task 2 commit

--- StepsTask2/StepsTask2.c
+++ StepsTask2/StepsTask2.c
@@ -36,9 +36,7 @@ void tokeniseRecord(const char *input, const char *delimiter,
     free(inputCopy);
 
                     }
-
-// Complete the main function
-int main() {
+int menu() {
     char filename [] = "FitnessData_2023.csv"; //open file
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
@@ -53,49 +51,65 @@ int main() {
 
     FITNESS_DATA records[100];
 
-    printf("Menu Options:");
-    printf("A: Specify the filename to be imported");
-    printf("B: Display the total number of records in the file");
-    printf("C: Find the date and time of the timeslot with the fewest steps");
-    printf("D: Find the date and time of the timeslot with the largest number of steps");
-    printf("E: Find the mean step count of all the records in the file");
-    printf("F: Find the longest continuous period where the step count is above 500 steps");
-    printf("Q: Quit");
-    printf("Enter choice: ")
+    printf("Menu Options:\n");
+    printf("A: Specify the filename to be imported\n");
+    printf("B: Display the total number of records in the file\n");
+    printf("C: Find the date and time of the timeslot with the fewest steps\n");
+    printf("D: Find the date and time of the timeslot with the largest number of steps\n");
+    printf("E: Find the mean step count of all the records in the file\n");
+    printf("F: Find the longest continuous period where the step count is above 500 steps\n");
+    printf("Q: Quit\n");
+    printf("Enter choice: \n")
 
     // get the next character typed in and store in the 'choice'
         choice = getchar();
 
         // this gets rid of the newline character which the user will enter
         // as otherwise this will stay in the stdin and be read next time
-        while (getchar() != '\n');
+        while (getchar() != '\n'); 
 
 
         // switch statement to control the menu.
         switch (choice)
         {
+        count = 0
         // this allows for either capital or lower case
         case 'A':
         case 'a':
-                
+            printf("Filename to be imported: %s", filename);
+            break;
 
+        case 'B':
+        case 'b':
+            while (fgets(line_buffer, buffer_size, file) != NULL) { //iterate through file one line at a time
+                if (count < 100) {
+                    count = count + 1;
+                }
+                printf("Number of records in file: %d\n", count);
+            break
 
-    while (fgets(line_buffer, buffer_size, file) != NULL) { //iterate through file one line at a time
-        if (count < 100) {
-            char date[11], time[6], steps[20]; //temporary variables
-            tokeniseRecord(line_buffer, ",", date, time, steps);
-            fdata.steps = atoi(steps);
-            strcpy(fdata.date, date);
-            strcpy(fdata.time,time);
-            records[count] = fdata;
-            count = count + 1;
-        }
+}
+// Complete the main function
+int main() {
+    menu();
+    
+
+//    while (fgets(line_buffer, buffer_size, file) != NULL) { //iterate through file one line at a time
+  //      if (count < 100) {
+    //        char date[11], time[6], steps[20]; //temporary variables
+      //      tokeniseRecord(line_buffer, ",", date, time, steps);
+        //    fdata.steps = atoi(steps);
+          //  strcpy(fdata.date, date);
+            //strcpy(fdata.time,time);
+            //records[count] = fdata;
+            //count = count + 1;
+     //   }
         
-    }
+  //  }
 
     fclose(file); //always remember to close the file again!
 
-    printf("Number of records in file: %d\n", count);
+  //  printf("Number of records in file: %d\n", count);
 
 
     return 0;
