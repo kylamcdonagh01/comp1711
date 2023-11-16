00467dfd81c366c434306604ab5370dcb48e7107
kylamcdonagh01 <146441430+kylamcdonagh01@users.noreply.github.com>
2023-11-16 10:52:23 +0000

    Add all of Steps task 2 work

--- StepsTask2/StepsTask2.c
+++ StepsTask2/StepsTask2.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include "FitnessDataStruct.h"
 
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
 // Struct moved to header file
 
 // Define any additional variables here
@@ -47,6 +54,8 @@ int menu() {
     int buffer_size = 100; //size of buffer determines how many characters are read from file
     char line_buffer[buffer_size];
     int count = 0;
+    char choice;
+    int smallest;
     FITNESS_DATA fdata; //creating array of FITNESS_DATA
 
     FITNESS_DATA records[100];
@@ -59,59 +68,66 @@ int menu() {
     printf("E: Find the mean step count of all the records in the file\n");
     printf("F: Find the longest continuous period where the step count is above 500 steps\n");
     printf("Q: Quit\n");
-    printf("Enter choice: \n")
+    printf("Enter choice: \n");
 
     // get the next character typed in and store in the 'choice'
         choice = getchar();
 
         // this gets rid of the newline character which the user will enter
         // as otherwise this will stay in the stdin and be read next time
-        while (getchar() != '\n'); 
+        while (getchar() != '\n');
 
 
         // switch statement to control the menu.
         switch (choice)
         {
-        count = 0
+        
         // this allows for either capital or lower case
         case 'A':
         case 'a':
-            printf("Filename to be imported: %s", filename);
+            printf("Filename to be imported: %s\n", filename);
             break;
 
         case 'B':
         case 'b':
+            count = 0;
             while (fgets(line_buffer, buffer_size, file) != NULL) { //iterate through file one line at a time
                 if (count < 100) {
                     count = count + 1;
                 }
-                printf("Number of records in file: %d\n", count);
-            break
+            }
+            printf("Number of records in file: %d\n", count);
+            break;
 
+        case 'C':
+        case 'c':
+            while (fgets(line_buffer, buffer_size, file) != NULL) { //iterate through file one line at a time
+                if (count < 100) {
+                    char date[11], time[6], steps[20]; //temporary variables
+                    tokeniseRecord(line_buffer, ",", date, time, steps);
+                    fdata.steps = atoi(steps);
+                    strcpy(fdata.date, date);
+                    strcpy(fdata.time,time);
+                    records[count] = fdata;
+                    count = count + 1;
+
+                    for (int i = 1; i < count && i < 100; i++) {
+                      if (records[i].steps < records[i-1].steps) {
+                        smallest = i;
+                      }
+                    }
+              printf("The fewest steps were completed on Date: %s at Time slot:/%s", records[smallest].date, records[smallest].time);
+        }
+
+                
+            
+    fclose(file); //always remember to close the file again!
+        }
+  }
 }
 // Complete the main function
 int main() {
     menu();
-    
-
-//    while (fgets(line_buffer, buffer_size, file) != NULL) { //iterate through file one line at a time
-  //      if (count < 100) {
-    //        char date[11], time[6], steps[20]; //temporary variables
-      //      tokeniseRecord(line_buffer, ",", date, time, steps);
-        //    fdata.steps = atoi(steps);
-          //  strcpy(fdata.date, date);
-            //strcpy(fdata.time,time);
-            //records[count] = fdata;
-            //count = count + 1;
-     //   }
-        
-  //  }
-
-    fclose(file); //always remember to close the file again!
-
-  //  printf("Number of records in file: %d\n", count);
-
-
     return 0;
 }
 
